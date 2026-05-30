fc422f9f46b280d0ac65ad48901cfeace2cbf313
DuvalK78 <duval.kerian18@ik.me>
2026-05-31 00:08:33 +0200

    Create MiniMap.tsx

---
+++ src/components/MiniMap.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix pour les icônes Leaflet
+const defaultIcon = L.icon({
+  iconUrl: "/images/marker-icon.png",
+  iconRetinaUrl: "/images/marker-icon-2x.png",
+  shadowUrl: "/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = defaultIcon;
+
+export default function MiniMap({
+  coordinates,
+  country,
+}: {
+  coordinates: [number, number];
+  country: string;
+}) {
+  return (
+    <div className="h-64 w-full rounded-lg overflow-hidden">
+      <MapContainer
+        center={coordinates}
+        zoom={5}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        />
+        <Marker position={coordinates}>
+          <Popup>{country}</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
